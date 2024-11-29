10b63e6d06982b6d8679a032ba605826374760d8
Ali <m.ali.rezaeiii09@gmail.com>
2024-11-29 19:11:36 +0330

    Initial Commit

---
+++ CachingSwiftData/CachingSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachingSwiftDataApp.swift
+//  CachingSwiftData
+//
+//  Created by Ali on 11/29/24.
+//
+
+import SwiftUI
+
+@main
+struct CachingSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
