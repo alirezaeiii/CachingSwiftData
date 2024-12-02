14e6f5821f9a49d5d75a931df14dc018571f34ae
Ali <m.ali.rezaeiii09@gmail.com>
2024-12-02 15:20:55 +0330

    init func in App struct

--- CachingSwiftData/CachingSwiftDataApp.swift
+++ CachingSwiftData/CachingSwiftDataApp.swift
@@ -19,15 +19,21 @@ struct CachingSwiftDataApp: App {
     private let networkService = NetworkService()
     private let modelContainer = try! ModelContainer(for: UserEntity.self)
     @State private var navigationPaths = [NavigationPath]()
+    @State private var viewModel: GithubViewModel
+    
+    init() {
+        let dataSource = UserDataSource(networkService: networkService, modelContext: modelContainer.mainContext)
+        viewModel = GithubViewModel(dataSource: dataSource)
+    }
     
     var body: some Scene {
         WindowGroup {
             NavigationStack(path: $navigationPaths) {
-                ContentView(viewModel: .init(dataSource: UserDataSource(networkService: networkService, modelContext: modelContainer.mainContext)), navigationPath: $navigationPaths)
+                ContentView(viewModel: viewModel, navigationPath: $navigationPaths)
                     .navigationDestination(for: NavigationPath.self) { path in
                         switch path {
                         case .list:
-                            ContentView(viewModel: .init(dataSource: UserDataSource(networkService: networkService, modelContext: modelContainer.mainContext)), navigationPath: $navigationPaths)
+                            ContentView(viewModel: viewModel, navigationPath: $navigationPaths)
                         case .detail(user:  let user):
                             DetailView(user: user)
                         }
